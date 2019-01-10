43cfcc6b605fdb5b8296fa04213750f4566df290
guoyangqiao <944734785@qq.com>
2019-01-10 17:00:50 +0800

    autocommit

--- popCount.go
+++ popCount.go
@@ -1,13 +1,14 @@
 package main
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+)
 
 var ru [256]byte
 
 func init() {
 	for k := range ru {
 		ru[k] = ru[k>>1] + byte(k&0x01)
-		println(ru[k])
 	}
 }
 
@@ -22,12 +23,16 @@ func PopCount(x uint64) int {
 		ru[byte(x>>(7*8))])
 }
 
-func BitCount(x, y [32]byte) int {
-
+func BitCount1(x, y []byte) int {
+	var sum byte
+	for i := 0; i < len(x); i++ {
+		sum += ru[byte(x[i]^y[i])]
+	}
+	return int(sum)
 }
-
 func main() {
-	c1 := sha256.Sum256([]byte("x"))
-	c2 := sha256.Sum256([]byte("X"))
-	BitCount(c1, c2)
+	c1 := sha256.Sum256([]byte("A"))
+	c2 := sha256.Sum256([]byte("B"))
+	count := BitCount1(c1[:], c2[:])
+	println(count)
 }
